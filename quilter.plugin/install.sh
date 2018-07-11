166800e727b054560a82e5ced876f5b94dd16a5e
Dick Thomas <dthomas@speaksthegeek.co.uk>
2018-07-11 19:48:27 +0100

    oops updated info this time

--- quilter.plugin/install.sh
+++ quilter.plugin/install.sh
@@ -2,10 +2,10 @@
 
 ##get gpg key
 wget -nv https://download.opensuse.org/repositories/home:speaksthegeek/Debian_9.0/Release.key -O Release.key
-apt-key add - < Release.key
-apt-get update
+apt add - < Release.key
+apt update
 
 ##add repo
 echo 'deb http://download.opensuse.org/repositories/home:/speaksthegeek/Debian_9.0/ /' > /etc/apt/sources.list.d/home:speaksthegeek.list
-apt-get update
-apt-get install com.github.lainsce.quilter
+apt update
+apt install -y com.github.lainsce.quilter
