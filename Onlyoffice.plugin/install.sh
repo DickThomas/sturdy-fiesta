1d16ad3a4fb32822ff0d8fc45944c25e96ebe514
Dick Thomas <dthomas@speaksthegeek.co.uk>
2018-07-11 20:38:10 +0100

    removed onlyoffice. icons don't show

--- Onlyoffice.plugin/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /tmp
-
-URL="https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb"
-wget $URL
-
-apg install -y onlyoffice-desktopeditors_amd64.deb
-apt -f install
--- Onlyoffice.plugin/uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-apt remove -y onlyoffice-desktopeditors
