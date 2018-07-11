93b8124e0b10b312980c5b97bbf91398a9fb8e26
Dick Thomas <dthomas@speaksthegeek.co.uk>
2018-07-11 21:12:20 +0100

    broken

--- pycharm.plugin/install.sh
+++ pycharm.plugin/install.sh
@@ -23,15 +23,16 @@ ln -sf /opt/pycharm/pycharm-community-2016.3.2/bin/pycharm.sh /usr/bin/pycharm
 xdg-icon-resource install --novendor --size 256 "/opt/pycharm/pycharm-community-2016.3.2/bin/pycharm.png" "pycharm"
 gtk-update-icon-cache -f -t /usr/share/icons/hicolor
 
-cat <<EOF | tee /usr/share/applications/pycharm.desktop
+
+cat <<EOF | tee /usr/share/applications/intellij.desktop
 [Desktop Entry]
-Name=pycharm Idea
+Name=pycharm
 Icon=pycharm
 Comment=The Most Intelligent python IDE
 Exec=/usr/bin/pycharm
 Terminal=false
 Type=Application
 StartupNotify=true
-Categories=Deveopment;Java
-Keywords=Java;Scala;Groovy;Deveopment
+Categories=Deveopment;python
+Keywords=python;Deveopment
 EOF
