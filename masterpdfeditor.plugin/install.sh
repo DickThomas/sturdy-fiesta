b3d22136e687b1b62689ce02f0b7e1f0d640472e
Dick Thomas <dthomas@speaksthegeek.co.uk>
2018-07-11 19:33:29 +0100

    quilter editor added

--- masterpdfeditor.plugin/install.sh
+++ masterpdfeditor.plugin/install.sh
@@ -4,11 +4,11 @@ aptitude install -y curl
 if [ `uname -m` = "x86_64" ]; then
 
   # 64-bit stuff here
-  curl http://get.code-industry.net/public/master-pdf-editor-4.0.60_qt5.amd64.deb  -o master-pdf-editor.deb
+  curl https://code-industry.net/public/master-pdf-editor-5.0.36_qt5.amd64.deb  -o master-pdf-editor.deb
 
 else
    # 32-bit stuff here
-  curl http://get.code-industry.net/public/master-pdf-editor-4.0.60_i386.deb -o master-pdf-editor.deb
+  curl https://code-industry.net/public/master-pdf-editor-5.0.36_i386.deb -o master-pdf-editor.deb
 fi
 
 dpkg -i master-pdf-editor.deb
