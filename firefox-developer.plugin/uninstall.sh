bbc10d95cc3b73b1f5ac57cd48a9f25a969a9759
Dick Thomas <dthomas@speaksthegeek.co.uk>
2018-07-12 19:13:50 +0100

    peek added

--- firefox-developer.plugin/uninstall.sh
+++ firefox-developer.plugin/uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-rm -f "/usr/bin/firefox-developer"
-rm -f "/usr/local/share/applications/firefox-developer.desktop"
-rm -rf "/opt/firefox-developer"
+rm -f "/usr/share/applications/peek.desktop"
+rm -rf "/opt/peek/"
