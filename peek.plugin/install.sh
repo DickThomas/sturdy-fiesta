dfe0f1ccafaf097a7d98e9c79c6a3186bb20821e
Dick Thomas <dthomas@speaksthegeek.co.uk>
2018-07-12 19:14:39 +0100

    peek update

--- peek.plugin/install.sh
+++ peek.plugin/install.sh
@@ -3,7 +3,7 @@
 curl https://github.com/phw/peek/releases/download/1.3.1/peek-1.3.1-0-x86_64.AppImage -o peek.AppImage
 mkdir -p /opt/peek
 mv peek.AppImage /opt/peek/
-cp /opt/cedema/plugins/peek.plugin/peek.png
+cp /opt/cedema/plugins/peek.plugin/peek.png /opt/peek/peek.png
 cat > /usr/share/applications/peek.desktop <<EOL
 [Desktop Entry]
 Version=1.0
@@ -11,7 +11,7 @@ Encoding=UTF-8
 Name=Peek
 Comment=Peek
 Exec=/opt/peek/peek.AppImage
-Icon=/opt/firefox/browser/chrome/icons/default/default128.png
+Icon=/opt/peek/peek.png
 Type=Application
 StartupNotify=true
 Keywords=Animation;recoreder;gif
